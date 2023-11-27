718c5509272efe1ce1d864f343f0b60f0118ad12
therealchrisbrown <ch.braun1@gmx.de>
2023-11-27 11:16:27 +0100

    added test code for sagemaker testing

--- sagemaker-test.py
+++ sagemaker-test.py
@@ -1,1 +1,23 @@
-## Sagemaker-Test code to be delivered
+import sagemaker
+
+sagemaker_session = sagemaker.session.Session()
+bucket = sagemaker_session.default_bucket()
+role=sagemaker.get_execution_role()
+
+prefix="myprefix"
+
+estimator = sagemaker.estimator.PyTorch(
+    framework_version="1.11.0",
+    py_version="py38",
+    role,
+    entry_point="train.py", #the entry point for your training code
+    source_dir="src", #path to your local folder containing your training code
+    instance_count=1,
+    instance_type="ml.g5.2xlarge",
+    hyperparameters={"epochs": 3}
+)
+
+train_config = sagemaker.TrainingInput('s3://{0}/{1}/train/'.format(bucket, prefix), content_type='text/csv')
+val_config = sagemaker.TrainingInput('s3://{0}/{1}/val/'.format(bucket, prefix), content_type='text/csv')
+
+estimator.fit({'train': train_config, 'validation': val_config })
